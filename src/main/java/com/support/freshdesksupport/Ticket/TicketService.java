aaa03917a7a32632959cd60abb431ae42e1b246b
Mohammed  Fazil <mohammedfazil.vamosys@gmail.com>
2021-01-28 12:39:13 +0530

    file rename commit

---
+++ src/main/java/com/support/freshdesksupport/Ticket/TicketService.java
@@ -0,0 +1,18 @@
+package com.support.freshdesksupport.Ticket;
+
+public class TicketService implements TicketServiceInterface
+{
+
+	@Override
+	public void getId(String id) {
+		
+		
+	}
+
+	@Override
+	public void getIssue(String issue) {
+		
+		
+	}
+	
+}
---
+++ src/main/java/com/support/freshdesksupport/Ticket/TicketServiceInterface.java
@@ -0,0 +1,8 @@
+package com.support.freshdesksupport.Ticket;
+
+public interface TicketServiceInterface
+{
+	public void getId(String id);
+	
+	public void getIssue(String issue);
+}
